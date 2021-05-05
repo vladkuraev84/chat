34c27011aaa922bd95eb6ef2818e45232432d742
vladkurev84 <vladkuraev84@gmail.com>
2021-05-06 02:23:49 +0300

    start4

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
-export const url = `${process.env.VUE_APP_SOCKET_HOST || 'http://localhost'}:${process.env.VUE_APP_SOCKET_PORT || '3000'}`
+// export const url = `${process.env.VUE_APP_SOCKET_HOST || 'http://localhost'}:${process.env.VUE_APP_SOCKET_PORT || '3000'}`
+export const url = `${process.env.VUE_APP_SOCKET_HOST || 'http://localhost' + (process.env.VUE_APP_SOCKET_PORT || '3000')}`
 
 export const STORE_ACTIONS = {
     joinRoom: 'joinRoom',
